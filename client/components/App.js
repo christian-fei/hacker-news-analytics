d6c519515279255554ab345ee10608154969e957
Christian Fei <crifei93@gmail.com>
2019-12-22 17:34:14 +0100

    client table

--- client/components/App.js
+++ client/components/App.js
@@ -24,10 +24,33 @@ export default class App extends Component {
 
   render () {
     const data = this.state.data
+    const pages = Object.keys(data).sort((a, b) => +a > +b)
     if (!data || Object.keys(data).length === 0) {
       return h('div', null, 'loading...')
     }
     return h('div', null, [
+      pages.map(p => {
+        return h('div', null, [
+          h('h1', null, `page ${p}`),
+          h('table', null, [
+            h('tr', null, [
+              h('th', null, 'rank'),
+              h('th', null, 'score'),
+              h('th', null, 'age'),
+              h('th', null, 'comments'),
+              h('th', null, 'title')
+            ]),
+            data[p].map(item => h('tr', null, [
+              h('td', null, '#' + item.rank),
+              h('td', null, `${item.score} upvotes`),
+              h('td', null, item.age),
+              h('td', null, `${item.commentCount} comments`),
+              h('td', null, h('a', { href: item.link, target: '_blank' }, item.title))
+            ])
+            )
+          ])
+        ])
+      }),
       h('pre', null, safeJSONstringify(data))
     ])
   }
