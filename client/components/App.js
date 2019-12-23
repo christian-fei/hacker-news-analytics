b4a3eed26a5e40204957e6df8f3eb17b0ed6503b
Christian Fei <crifei93@gmail.com>
2019-12-23 20:46:26 +0100

    client with charts

--- client/components/App.js
+++ client/components/App.js
@@ -31,22 +31,40 @@ export default class App extends Component {
     }
 
     if (this.state.isStats) {
+      const chartHeight = 100
+
+      const scores = data.map(d => d.score).reverse()
+      const maxScore = Math.max(...scores)
+      const minScore = Math.min(...scores)
+      const normalizedScores = scores.map(score => ((score - minScore) / (maxScore - minScore)) * chartHeight)
+      const comments = data.map(d => d.commentCount).reverse()
+      const maxComments = Math.max(...comments)
+      const minComments = Math.min(...comments)
+      const normalizedComments = comments.map(comment => ((comment - minComments) / (maxComments - minComments)) * chartHeight)
       return h('div', null, [
         h('div', null, [
+          h('h4', null, 'score over time'),
+          h('br', null, []),
+          h('svg', { width: '600px', height: `${chartHeight}px` }, normalizedScores.map((n, i) => h('rect', { width: `${100 / normalizedScores.length}%`, x: 0, y: chartHeight - n, height: n, transform: `translate(${600 / normalizedScores.length * i}, 0)` }))),
+          h('h4', null, 'comments over time'),
+          h('br', null, []),
+          h('svg', { width: '600px', height: `${chartHeight}px` }, normalizedComments.map((n, i) => h('rect', { width: `${100 / normalizedComments.length}%`, x: 0, y: chartHeight - n, height: n, transform: `translate(${600 / normalizedComments.length * i}, 0)` }))),
+          h('h4', null, 'changes over time'),
+          h('br', null, []),
           h('table', null, [
             h('tr', null, [
               h('th', null, 'rank'),
-              h('th', null, 'score'),
               h('th', null, 'age'),
+              h('th', null, 'score'),
               h('th', null, 'comments'),
               h('th', null, 'title'),
               h('th', null, 'updated')
             ]),
             data.map(item => h('tr', { id: item.id, onClick: (e) => { if (!this.state.isStats) window.location.href = window.location.href.replace(/$/, `stats/${item.id}`) } }, [
               h('td', null, '#' + item.rank),
-              h('td', null, `${item.score} upvotes`),
               h('td', null, item.age),
-              h('td', null, `${item.commentCount} comments`),
+              h('td', null, `${item.score}`),
+              h('td', null, `${item.commentCount}`),
               h('td', null, h('a', { href: item.link, target: '_blank' }, item.title)),
               h('td', null, item.updated)
             ]))
@@ -62,16 +80,16 @@ export default class App extends Component {
           h('table', null, [
             h('tr', null, [
               h('th', null, 'rank'),
-              h('th', null, 'score'),
               h('th', null, 'age'),
+              h('th', null, 'score'),
               h('th', null, 'comments'),
               h('th', null, 'title')
             ]),
             data[p].map(item => h('tr', { id: item.id, onClick: (e) => { window.location.href = window.location.href.replace(/$/, `stats/${item.id}`) } }, [
               h('td', null, '#' + item.rank),
-              h('td', null, `${item.score} upvotes`),
               h('td', null, item.age),
-              h('td', null, `${item.commentCount} comments`),
+              h('td', null, `${item.score}`),
+              h('td', null, `${item.commentCount}`),
               h('td', null, h('a', { href: item.link, target: '_blank' }, item.title))
             ])
             )
