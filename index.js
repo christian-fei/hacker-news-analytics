cca2bcb3fbc209e6364b97c54c8b63933197e0a9
Christian Fei <crifei93@gmail.com>
2019-12-29 11:07:18 +0100

    no limit

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ async function createServer ({ port = process.env.PORT || process.env.HTTP_PORT 
           res.write('event: message\n')
           const id = req.url.replace(/\/stats\//gi, '')
           console.log('finding items', { id })
-          const data = await itemsColl.find({ id }, { limit: 60 * 24, sort: { updated: -1 } })
+          const data = await itemsColl.find({ id }, { sort: { updated: -1 } })
           res.write('event: message\n')
           res.write(`data: ${JSON.stringify({ time: new Date().toISOString(), data })}\n`)
           return res.write('\n\n')
